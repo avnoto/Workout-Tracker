489697a9cc657672a607d97e6ca8066f08b1d58b
avnoto <alexandravnoto@gmail.com>
2020-08-04 10:56:40 -0700

    added function in model to display total duration instead of undefined

--- models/workout.js
+++ models/workout.js
@@ -39,6 +39,12 @@ const workoutSchema = new Schema({
   ],
 });
 
+workoutSchema.virtual('totalDuration').get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
 const Workout = mongoose.model('Workout', workoutSchema);
 
 module.exports = Workout;
